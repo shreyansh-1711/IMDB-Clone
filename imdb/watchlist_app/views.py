e1c67e9a4eeca80fe532a22e1e50009fa738c660
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-02-17 01:14:59 +0530

    Json response

--- imdb/watchlist_app/views.py
+++ imdb/watchlist_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
+
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies' : list(movies.values())
+    }
+    # print(movies.values())
+    return JsonResponse(data)
+
+
+def movie_detail(request, pk):
+    # pk = primary key 
+    
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name' : movie.name,
+        'description' : movie.description,
+        'active' : movie.active
+    }
+    print(movie)
+    return JsonResponse(data)
+    
+    
