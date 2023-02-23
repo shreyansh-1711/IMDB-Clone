69b785bc555682796c491163c1dc6f9941228514
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-02-24 01:27:34 +0530

    Serializer CRUD

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from watchlist_app.views import movie_list, movie_detail
+from watchlist_app.api.views import movie_list, movie_detail
 
 urlpatterns = [
     path('list/', movie_list, name='movie_list'),
