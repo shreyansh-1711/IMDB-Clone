0af6a25d5e89ad6ade906069f9a96decbdce6d83
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-02-27 21:59:32 +0530

    validations

--- imdb/watchlist_app/api/serializers.py
+++ imdb/watchlist_app/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+
+def name_length( value):
+    if len(value) <2 :
+        raise serializers.ValidationError("Name must be at least 2 characters")
+  
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators = [name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
     
@@ -16,5 +22,24 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance 
+    
+    
+    # Object level Validation 
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and description cannot be same")
+        else:
+            return data
+    
+    
+    # Feild-level Validation 
+    # def validate_name(self, value):
+    #     if len(value) <2 :
+    #         raise serializers.ValidationError("Name must be at least 2 characters")
+    #     else:
+    #         return value
+    
+    
+            
         
     
