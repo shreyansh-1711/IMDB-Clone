acfdaed77f29787bcbcc12373df3cbbe61a139cb
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-02 01:27:03 +0530

    Generic class

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_detail
-from watchlist_app.api.views import WatchListAV, WatchListDetailAV,StreamPlatformAV,StreamPlatformDetailAV
+from watchlist_app.api.views import (WatchListAV, WatchListDetailAV,StreamPlatformAV
+                                     ,StreamPlatformDetailAV,ReviewCreate, ReviewList,ReviewDetail)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie_list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    
+    # path('review/', ReviewList.as_view(), name='review'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
+    
+    path('stream/<int:pk>/review-create',  ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review',  ReviewList.as_view(), name='review'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
--- imdb/watchlist_app/api/views.py
+++ imdb/watchlist_app/api/views.py
@@ -1,11 +1,57 @@
 from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from watchlist_app.models import WatchList,StreamPlatform
+from rest_framework import generics
+# from rest_framework import mixins
 from rest_framework import status
+
+
+from watchlist_app.models import WatchList,StreamPlatform, Review
 from watchlist_app.api.serializers import WatchListSerializer,StreamPlatformSerializer, ReviewSerializer
 
 
+class ReviewCreate(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        watchlist = WatchList.objects.get(pk=pk)
+        
+        serializer.save(watchlist=watchlist)
+        
+        
+
+class ReviewList(generics.ListCreateAPIView):
+    serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(watchlist=pk)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer 
+
+# class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+  
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)  
+    
+
+
+# class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
+    
+    
 class StreamPlatformAV(APIView):
     def get (self, request):
         platform = StreamPlatform.objects.all()
