226ee48196eeba7090a3e95e9511679e8680db3f
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-03 11:41:27 +0530

    TEmo login logout

--- imdb/watchlist_app/api/views.py
+++ imdb/watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework .exceptions import ValidationError
 # from rest_framework import mixins
 from rest_framework import status
 from rest_framework import viewsets
@@ -14,11 +15,20 @@ from watchlist_app.api.serializers import WatchListSerializer,StreamPlatformSeri
 
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    def get_queryset(self):
+        return Review.objects.all()
+    
+    
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user = review_user)
+        if(review_queryset.exists()):
+            raise ValidationError("Already have a review")
+            
         
-        serializer.save(watchlist=watchlist)
+        serializer.save(watchlist=watchlist, review_user=review_user)
         
         
 
@@ -53,17 +63,33 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 #         return self.create(request, *args, **kwargs)
     
 
-class StreamPlatformVS(viewsets.ViewSet):
-    def list(self, request):
-        queryset = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(queryset, many=True)
-        return Response(serializer.data)
+# class StreamPlatformVS(viewsets.ViewSet):
+#     def list(self, request):
+#         queryset = StreamPlatform.objects.all()
+#         serializer = StreamPlatformSerializer(queryset, many=True)
+#         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = StreamPlatform.objects.all()
-        watchlist = get_object_or_404(queryset, pk=pk)
-        serializer = StreamPlatformSerializer(watchlist)
-        return Response(serializer.data)
+#     def retrieve(self, request, pk=None):
+#         queryset = StreamPlatform.objects.all()
+#         watchlist = get_object_or_404(queryset, pk=pk)
+#         serializer = StreamPlatformSerializer(watchlist)
+#         return Response(serializer.data)
+    
+#     def create(self, request):
+#         serializer = StreamPlatformSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)    
+#         else:
+#             return Response(serializer.errors)
+        
+        
+class StreamPlatformVS(viewsets.ModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
+    
+
+
     
 class StreamPlatformAV(APIView):
     def get (self, request):
@@ -78,6 +104,9 @@ class StreamPlatformAV(APIView):
             return Response(serializer.data)    
         else:
             return Response(serializer.errors)
+        
+    
+            
         
         
 class StreamPlatformDetailAV(APIView):  
--- imdb/watchlist_app/models.py
+++ imdb/watchlist_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -27,6 +27,7 @@ class WatchList(models.Model):
 
 
 class Review(models.Model):
+    review_user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=True)
     description = models.CharField(max_length=200, null=True)
     watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
