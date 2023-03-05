8ab11858a10b27ff04bd818204a8b9dd7fa10d3d
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-06 00:30:42 +0530

    JWT done

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -136,6 +136,11 @@ REST_FRAMEWORK = {
     #     'rest_framework.authentication.BasicAuthentication',
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    'ROTATE_REFRESH_TOKENS': True,
 }
