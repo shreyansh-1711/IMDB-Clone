d9a111c38da06e60ab8004a94647ad3f003c0f4e
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-08 23:21:22 +0530

    pagination

--- imdb/watchlist_app/api/pagination.py
+++ imdb/watchlist_app/api/pagination.py
@@ -2,3 +2,8 @@ from rest_framework.pagination import PageNumberPagination
 
 class WatchListPagination(PageNumberPagination):
     page_size = 5
+    page_query_param = 'p'
+    page_size_query_param = 'size'
+    max_page_size = 10
+    
+    
