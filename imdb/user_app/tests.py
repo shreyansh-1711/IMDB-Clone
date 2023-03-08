2af1bcbf6dedf68e6f6e4048eb051444e730f298
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-09 00:38:10 +0530

    login logout testcase

--- imdb/user_app/tests.py
+++ imdb/user_app/tests.py
@@ -22,4 +22,23 @@ class RegisterTestCase(APITestCase):
 
 class LoginLogoutTestCase(APITestCase):
     
-            
+    def setUp(self):
+        self.user = User.objects.create_user(username="testcase", password="Testcase@123")
+        
+    def test_login(self):
+        data = {
+            "username": "testcase",
+            "password": "Testcase@123",
+        }    
+        
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username="testcase")
+        self.client.credentials(HTTP_AUTHORIZATION = 'Token '+self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User     
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from watchlist_app.api import serializers
+from watchlist_app import models
+
+
+class StreamPlatformTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="testcase", password="Testcase@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION = 'Token '+self.token.key)
+        
+    
+    def test_streamplatform_create(self):
+        data = {
+            "name": "Netlfix",
+            "about": "#1 stream platform",
+            "website": "https://netflix.com",
+        }
+        
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
